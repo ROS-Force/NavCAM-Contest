848d8bd5046b97e99bdf22068d32a3d349bca38d
mjpc13 <mjpc13@gmail.com>
2021-01-03 17:29:17 +0000

    Adding edge subtraction

--- catkin_ws/src/object_detection/src/falling_object.py
+++ catkin_ws/src/object_detection/src/falling_object.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+from logging import error
 import rospy
 import math
 import sys
@@ -10,6 +11,7 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+import time
 
 
 class Falling_Object():
@@ -21,7 +23,8 @@ class Falling_Object():
         #Publisher
         self.pub = rospy.Publisher("/detection/moving_object", Image, queue_size=10)
 
-        self.sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.imageCallback)
+        #self.sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.imageCallback, queue_size=1)
+        self.sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imageCallback, queue_size=1)
 
 
     
@@ -30,18 +33,34 @@ class Falling_Object():
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
 
-            #cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-            cv_image_gray = cv2.GaussianBlur (cv_image, (31, 31), 0)
+
+            cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
+            edge = cv2.Canny(cv_image_gray, 100,200)
 
             try:
-                dif_img = cv2.absdiff(self.previous_frame, cv_image_gray)
-                self.previous_frame = cv_image_gray
-                image_message = self.bridge.cv2_to_imgmsg(dif_img, encoding="passthrough")
+                
+                dif_img = cv2.absdiff(self.previous_frame, edge)
+                self.previous_frame = edge
+
+                #2 iterations
+                dif_img_blur = cv2.GaussianBlur (dif_img, (31, 31), 0)
+                dif_img_blur = cv2.GaussianBlur (dif_img_blur, (11, 11), 0)
+
+                ret, test = cv2.threshold(dif_img_blur, 25, 255, 0)
+
+                contours, hierarchy = cv2.findContours(test, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+                cv2.drawContours(cv_image, contours, -1, (0,255,0), 1)
+
+                #image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
+                image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
+
                 self.pub.publish(image_message)
+                time.sleep(0.01)
+
 
             except:
-                print(type(cv_image_gray))
-                self.previous_frame = cv_image_gray
+                self.previous_frame = edge
 
             
         except CvBridgeError as e:
