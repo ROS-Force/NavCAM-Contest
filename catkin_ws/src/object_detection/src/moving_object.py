4ae4032c5adc79a94f96ba23da648787e9acaa27
mjpc13 <mjpc13@gmail.com>
2021-01-04 19:30:17 +0000

    Experimentar Laplaciano, criar mascaras com a depth para remover background

--- catkin_ws/src/object_detection/src/moving_object.py
+++ catkin_ws/src/object_detection/src/moving_object.py
@@ -19,12 +19,14 @@ class Moving_Object():
     def __init__(self):
 
         self.bridge = CvBridge()
+        self.countFrame = 1;
 
         #Publisher
         self.pub = rospy.Publisher("/detection/moving_object", Image, queue_size=10)
+        self.pub_depth = rospy.Publisher("/detection/moving_object_depth", Image, queue_size=10)
 
-        #self.sub = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.imageCallback, queue_size=1)
-        self.sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imageCallback, queue_size=1)
+        self.sub_depth = rospy.Subscriber("/camera/depth/image_rect_raw", Image, self.imageCallbackDepth, queue_size=10)
+        self.sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imageCallback, queue_size=10)
 
 
     
@@ -33,35 +35,29 @@ class Moving_Object():
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)
 
-
             cv_image_gray = cv2.cvtColor(cv_image, cv2.COLOR_RGB2GRAY)
-            edge = cv2.Canny(cv_image_gray, 100,200)
+            #edge = cv2.Laplacian(cv_image_gray,cv2.CV_8UC1)
+            edge = cv2.Canny(cv2.GaussianBlur (cv_image_gray, (7, 7), 0), 120,200)
 
             try:
                 
                 dif_img = cv2.absdiff(self.previous_frame, edge)
                 self.previous_frame = edge
 
-                #2 iterations
-                dif_img_blur = cv2.GaussianBlur (dif_img, (31, 31), 0)
-                dif_img_blur = cv2.GaussianBlur (dif_img_blur, (11, 11), 0)
 
+                dif_img_blur = cv2.GaussianBlur (dif_img, (9, 9), 0)
                 ret, test = cv2.threshold(dif_img_blur, 25, 255, 0)
 
-                contours, hierarchy = cv2.findContours(test, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
-
+                contours, hierarchy = cv2.findContours(test, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2:]
                 cv2.drawContours(cv_image, contours, -1, (0,255,0), 1)
+                    
 
-                #image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
                 image_message = self.bridge.cv2_to_imgmsg(cv_image, encoding="passthrough")
-
                 self.pub.publish(image_message)
-                time.sleep(0.01)
-
 
             except:
+                    
                 self.previous_frame = edge
-
             
         except CvBridgeError as e:
             print(e)
@@ -69,6 +65,42 @@ class Moving_Object():
         except ValueError as e:
             print(e)
             return
+        
+
+    def imageCallbackDepth(self, data):
+
+
+        try:
+            new_data = self.bridge.imgmsg_to_cv2(data, data.encoding)
+
+            ret, high_thr = cv2.threshold(new_data, 6000, 255, 1)
+            ret, low_thr = cv2.threshold(new_data, 20, 255, 0)
+            #img = cv2.bitwise_and(new_data, new_data, mask = test)
+            cv_image = cv2.convertScaleAbs(new_data)
+
+
+            try:
+
+                
+                dif_img = cv2.absdiff(self.previous_frame_depth, cv_image)
+                self.previous_frame_depth = cv_image
+                dif_img_blur = cv2.GaussianBlur (dif_img, (21, 21), 0)
+                
+
+                image_message = self.bridge.cv2_to_imgmsg(low_thr, encoding="passthrough")
+
+                self.pub_depth.publish(image_message)
+
+            except:
+                self.previous_frame_depth = cv_image
+
+        except CvBridgeError as e:
+            print(e)
+            return
+        except ValueError as e:
+            print(e)
+            return
+
 
 
     def run(self):
