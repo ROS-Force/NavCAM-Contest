0a231a03713a19f409a967cba5960261cfea9451
mjpc13 <mjpc13@gmail.com>
2021-01-24 17:20:53 +0000

    Import da Class Sort, e processar as bbox

--- catkin_ws/src/object_detection/src/sort_tracking.py
+++ catkin_ws/src/object_detection/src/sort_tracking.py
@@ -5,13 +5,13 @@ import math
 import sys
 import numpy as np
 import cv2
+import imp
 
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 from object_detection.msg import bbox_msgs
-from .sort import Sort
 import rospkg
 
 class Sort_tracking():
@@ -19,12 +19,15 @@ class Sort_tracking():
     def __init__(self):
 
         self.bridge = CvBridge()
-        self.mo_tracker = Sort()
+        abs_path = rospkg.RosPack().get_path("object_detection") + "/src/sort/"
+    
+        si = imp.load_source('sort.sort', abs_path + 'sort.py')
+        self.mo_tracker = si.Sort()
 
 
         #Publisher
         self.pub = rospy.Publisher("/detection/yolo/objects_image", Image, queue_size=1)
-        self.pub_bbox = rospy.Publisher("/detection/yolo/bbox", bbox_msgs, queue_size=10)
+        self.sub_bbox = rospy.Subscriber("/detection/yolo/bbox", bbox_msgs,self.bboxCallback, queue_size=10)
         self.sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imageCallback, queue_size=1, buff_size=2**24)
 
     def imageCallback(self, data):
@@ -37,6 +40,19 @@ class Sort_tracking():
         except ValueError as e:
             print(e)
             return
+    
+    def bboxCallback(self,data):
+        
+        flatten_bbox = data.bbox_list
+        flatten_bbox = np.asarray(flatten_bbox) 
+        
+        classid_list = data.classid
+
+        self.list_bbox = flatten_bbox.reshape(-1,5)
+
+        print(self.list_bbox)
+        print(len(classid_list))
+
 
 
     def run(self):
