5f8e12c496f7e2c9669da96f6c97484b0d4d61ca
Ch0s3n0ne <huhu.gocosta@gmail.com>
2021-03-03 00:18:22 +0000

    inicio codigo de seguimento de paredes

---
+++ catkin_ws/src/wall_follower/src/following_walls.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#from __future__ import print_function
+##################################### Header ############################################
+""" obstruction.py: Description of the node """
+#__author__ = ""
+#__credits__ = [""]
+#__version__ = "0.0.0"
+#__maintainer__ = ""
+#__email__ = ""
+#########################################################################################
+# import any libraries necessary to run script
+
+import rospy
+import numpy as np
+import rospkg
+import pyrealsense2 as rs2
+
+from std_msgs.msg import Header
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CameraInfo
+from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Twist
+
+class following_walls():
+
+    def __init__(self):
+
+
+        #Initialize variables
+        self.pub_velocities=rospy.Publisher("cmd_vel", Twist ,queue_size=10)
+
+        self.sub_info = rospy.Subscriber("/camera/aligned_depth_to_color/camera_info", CameraInfo, self.imageDepthInfoCallback)
+
+        self.sub = rospy.Subscriber("/camera/color/image_raw", Image, self.imageCallback, queue_size=1, buff_size=2**24)
+
+        self.sub_depth = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.imageDepthCallback, queue_size=1, buff_size=2**24)
+
+
+#Callbacks
+    def imageCallback(self, data): #Function that runs when a RGB image arrives
+        try:
+            self.data_encoding = data.encoding #Stores the encoding of original image
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, data.encoding)  # Transforms the format of image into OpenCV 2
+
+        except CvBridgeError as e:
+            print(e)
+            return
+        except ValueError as e:
+            print(e)
+            return
+
+
+    def imageDepthCallback(self, data): #Function that runs when a Depth image arrives
+        try:
+            self.data_encoding_depth = data.encoding
+            self.cv_image_depth = self.bridge.imgmsg_to_cv2(data, data.encoding) # Transforms the format of image into OpenCV 2
+            rospy.loginfo(self.cv_image_depth)
+
+        except CvBridgeError as e:
+            print(e)
+            return
+        except ValueError as e:
+            print(e)
+            return
+
+
+    def imageDepthInfoCallback(self, cameraInfo): #Code copied from Intel script "show_center_depth.py". Gather camera intrisics parameters that will be use to compute the real coordinates of pixels
+        try:
+            if self.intrinsics:
+                return
+            self.intrinsics = rs2.intrinsics()
+            self.intrinsics.width = cameraInfo.width
+            self.intrinsics.height = cameraInfo.height
+            self.intrinsics.ppx = cameraInfo.K[2]
+            self.intrinsics.ppy = cameraInfo.K[5]
+            self.intrinsics.fx = cameraInfo.K[0]
+            self.intrinsics.fy = cameraInfo.K[4]
+            if cameraInfo.distortion_model == 'plumb_bob':
+                self.intrinsics.model = rs2.distortion.brown_conrady
+            elif cameraInfo.distortion_model == 'equidistant':
+                self.intrinsics.model = rs2.distortion.kannala_brandt4
+            self.intrinsics.coeffs = [i for i in cameraInfo.D]
+        except CvBridgeError as e:
+            print(e)
+            return
+
+def main():
+
+    rospy.init_node('following_walls')
+    st = following_walls()
+    rospy.spin()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        print("error")
+        pass
+    print("Exiting process...")
