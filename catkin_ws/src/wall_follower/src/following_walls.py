1d0e1839c322c309126f1ed17aae6f9f3cb35c4b
Ch0senOne <huhu.gocosta@gmail.com>
2021-03-11 17:23:33 +0000

    2º version following_walls

--- catkin_ws/src/wall_follower/src/following_walls.py
+++ catkin_ws/src/wall_follower/src/following_walls.py
@@ -35,7 +35,10 @@ class following_walls():
 
 
         self.bridge = CvBridge()
-        self.rate = rospy.Rate(100)
+        self.rate = rospy.Rate(10)
+
+        wall_in_the_right=True
+        target_distance=1000
 
         #self.sub_info = rospy.Subscriber("/camera/aligned_depth_to_color/camera_info", CameraInfo, self.imageDepthInfoCallback)
 
@@ -43,10 +46,60 @@ class following_walls():
 
         self.sub_depth = rospy.Subscriber("/camera/aligned_depth_to_color/image_raw", Image, self.imageDepthCallback, queue_size=1, buff_size=2**24)
 
+        self.sub_info = rospy.Subscriber("/camera/aligned_depth_to_color/camera_info", CameraInfo, self.imageDepthInfoCallback)
 
         while not rospy.is_shutdown():
 
+            #left_side_distance
+            i1=self.top_limit
+            j1=0
+            x_left=[]
+
+            while i1<=self.bottom_limit:
+                while j1<=self.left_squadron_limit:
+
+                    depth = self.cv_image_depth[pix[i1], pix[j1]] #Depth of the central pixel
+                    result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[i1], pix[j1]], depth) # Real coordenates, in mm, of the central pixel
+
+                    #Create a vector with the coordinates, in meters
+                    x_left.append(result[0])
+                    j1=j1+1
+
+                i1=i1+1
+
+            distance_left=sum(x_left)/len(x_left)
+
+            i2=self.top_limit
+            j2=self.rigth_squadron_limit
+            x_right=[]
             
+            while i2<=self.bottom_limit:
+                while j2<=self.pixel_width:
+
+                    depth = self.cv_image_depth[pix[i2], pix[j2]] #Depth of the central pixel
+                    result = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[i2], pix[j2]], depth) # Real coordenates, in mm, of the central pixel
+
+                    #Create a vector with the coordinates, in meters
+                    x_right.append(result[0])
+                    j2=j2+1
+
+                i2=i2+1
+
+            distance_right=sum(x_right)/len(x_right)
+
+            if(wall_in_the_right):
+
+                if(distance_right<=target_distance):
+
+                    #take action
+            
+            else:
+                
+                 if(distance_left<=target_distance):
+
+                    #take action
+
+
 
             self.rate.sleep()
 
@@ -70,10 +123,10 @@ class following_walls():
                     self.angle_diagonal=89
 
 
-                    self.left_squadron=self.pixel_width/4
-                    self.rigth_squadron=1*self.pixel_width/4
-                    self.top_limit=self.pixel_height/3
-                    self.bottom_limit=2*self.pixel_height/3
+                    self.left_squadron=self.pixel_width/8
+                    self.rigth_squadron=7*self.pixel_width/8
+                    self.top_limit=self.pixel_height/6
+                    self.bottom_limit=5*self.pixel_height/6
 
                     rospy.loginfo("Low Resolution")
 
@@ -85,28 +138,63 @@ class following_walls():
 
                     rospy.loginfo("High Resolution")
 
-                self.pixel_angle_horizontal_incrementation=self.angle_horizontal/self.pixel_width
-                self.pixel_angle_height_incrementation=self.angle_height/self.pixel_height
+                #self.pixel_angle_horizontal_incrementation=self.angle_horizontal/self.pixel_width
+                #self.pixel_angle_height_incrementation=self.angle_height/self.pixel_height
 
                 self.left_squadron_limit=int(self.left_squadron)
                 self.rigth_squadron_limit=int(self.rigth_squadron)
                 self.top_limit=int(self.top_limit)    
                 self.bottom_limit=int(self.bottom_limit)
                 
-                self.angle_horizontal=90-(self.angle_horizontal/2)
-                self.angle_height=90-(self.angle_height/2)
+                #self.angle_horizontal=90-(self.angle_horizontal/2)
+                #self.angle_height=90-(self.angle_height/2)
                           
                 self.check_resolution=False
 
+                except CvBridgeError as e:
+                    print(e)
+                    return
+                except ValueError as e:
+                    print(e)
+                    return
+
+        def imageDepthInfoCallback(self, cameraInfo): #Code copied from Intel script "show_center_depth.py". Gather camera intrisics parameters that will be use to compute the real coordinates of pixels
+            try:
+                if self.intrinsics:
+                    return
+                self.intrinsics = rs2.intrinsics()
+                self.intrinsics.width = cameraInfo.width
+                self.intrinsics.height = cameraInfo.height
+                self.intrinsics.ppx = cameraInfo.K[2]
+                self.intrinsics.ppy = cameraInfo.K[5]
+                self.intrinsics.fx = cameraInfo.K[0]
+                self.intrinsics.fy = cameraInfo.K[4]
+                if cameraInfo.distortion_model == 'plumb_bob':
+                    self.intrinsics.model = rs2.distortion.brown_conrady
+                elif cameraInfo.distortion_model == 'equidistant':
+                    self.intrinsics.model = rs2.distortion.kannala_brandt4
+                self.intrinsics.coeffs = [i for i in cameraInfo.D]
+            except CvBridgeError as e:
+                print(e)
+                return
+
+        def computeRealCenter(self, tracker):
+            
+            center = Vector3()
+
+            pix = [int(tracker.xmin + (tracker.xmax-tracker.xmin)//2), int(tracker.ymin + (tracker.ymax-tracker.ymin)//2)] #Coordinates of the central point (in pixeis)
+            depth = self.cv_image_depth[pix[1], pix[0]] #Depth of the central pixel
+            depthresult = rs2.rs2_deproject_pixel_to_point(self.intrinsics, [pix[0], pix[1]], depth) # Real coordenates, in mm, of the central pixel
+
+            #Create a vector with the coordinates, in meters
+            center.x = result[0]*10**(-3)
+            center.y = result[1]*10**(-3)
+            center.z = result[2]*10**(-3)
+
+        return center
 
 
-
-        except CvBridgeError as e:
-            print(e)
-            return
-        except ValueError as e:
-            print(e)
-            return
+        
 
 def main():
 
