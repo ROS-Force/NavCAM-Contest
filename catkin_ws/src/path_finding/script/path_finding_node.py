d7136884adef41946ae9811d5a7a10faaa9c3ba2
jncfa <20467009+jncfa@users.noreply.github.com>
2021-04-30 11:54:00 +0100

    WTF MARIO

--- catkin_ws/src/path_finding/script/path_finding_node.py
+++ catkin_ws/src/path_finding/script/path_finding_node.py
@@ -5,18 +5,18 @@ import numpy as np
 import cv2
 import pyrealsense2 as rs2
 import sys
-
+import tf
 from std_msgs.msg import Header
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CameraInfo
 from geometry_msgs.msg import Pose
-
 from nav_msgs.msg import OccupancyGrid, MapMetaData
+from nav_msgs.srv import GetMap
 
 from pathfinding.core.diagonal_movement import DiagonalMovement
 from pathfinding.core.grid import Grid
 from pathfinding.finder.a_star import AStarFinder
-
+from datetime import datetime
 
 class Path_finding():
 
@@ -26,97 +26,139 @@ class Path_finding():
 
         self.intrinsics = None
         self.bridge = CvBridge()
+        self.path = None
+        self.stopPathFinding = False        
 
         #Publishers
         #self.pub = rospy.Publisher("output_image", Image, queue_size=1)
         #self.pub_obj = rospy.Publisher("object_info", Object_info, queue_size=1)
 
-        #Subscriber
-        self.sub = rospy.Subscriber("map", OccupancyGrid, self.mapCallback, queue_size=10, buff_size=2**24)
+        self.tflistener = tf.TransformListener() 
 
-        #Publisher
+        self.finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
+
+        #Subscriber
+        #self.sub = rospy.Subscriber("map", OccupancyGrid, self.mapCallback, queue_size=10, buff_size=2**24)
+
+        #self.sub = rospy.Subscriber("goal", OccupancyGrid, self.mapCallback, queue_size=10, buff_size=2**24)
+
+        #Publisher (update this )
         self.pub = rospy.Publisher("path_image", Image, queue_size=1)
 
 
 
+    def getCurrentMap(self):
+        rospy.wait_for_service('dynamic_map')
+        try:
+            dynamicMap = rospy.ServiceProxy('dynamic_map', GetMap)
+            dynamicMapResult = dynamicMap()
+
+            return dynamicMapResult.map
+        except rospy.ServiceException as e:
+            print("Service call failed: %s"%e)
+
 #Callbacks
-    def mapCallback(self, data): #Function that runs when a RGB image arrives
-        header = data.header
+    def pathUpdateCallback(self): 
+        print(datetime.now().timestamp(), "before map")
 
-        origin = data.info.origin
-        resolution = data.info.resolution
-        map_width = data.info.width
-        map_height = data.info.height
+        latestGrid = self.getCurrentMap()
 
-        map_array = np.reshape(data.data, [map_height, map_width])
+        print(datetime.now().timestamp(), "after map")
+        
+        header = latestGrid.header
 
-        map_array = self.cropMap(map_array)
+        origin = latestGrid.info.origin
+        resolution = latestGrid.info.resolution
+        map_width = latestGrid.info.width
+        map_height = latestGrid.info.height
 
+        map_array = np.reshape(latestGrid.data, [map_height, map_width])
+        #map_array = self.cropMap(map_array)
         map_array[map_array == 0] = 1
         map_array[map_array == 100] = -1
 
-
         grid = Grid(matrix=map_array)
+        
+        #try:
+        #    (trans,rot) = self.tflistener.lookupTransform('/camera_link', '/map', rospy.Time(0))
+        #except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+        #    print("Error on transform")
+        #    pass
 
-
-        start = (map_array.shape[0]//2, map_array.shape[1]//2)
-        end = (map_array.shape[0]//2+10, map_array.shape[1]//2-60)
+        # set
+        start = np.array([map_array.shape[0]//2, map_array.shape[1]//2])
+        end = start + 5
 
         start_node = grid.node(start[0],start[1])
         end_node = grid.node(end[0], end[1])
 
+        print(datetime.now().timestamp(), "before A star")
+        
+        path, runs = self.finder.find_path(start_node, end_node, grid)
 
-        finder = AStarFinder(diagonal_movement=DiagonalMovement.always)
-        path, runs = finder.find_path(start_node, end_node, grid)
-        print('operations:', runs, 'path length:', len(path))
+        
+        print(datetime.now().timestamp(), "before map conv")
+        #print('operations:', runs, 'path length:', len(path))
 
         image = self.map2Image(map_array, start, end, path)
+        self.path = path
 
+        print(datetime.now().timestamp(), "after map conv")
+        
         image_message = self.bridge.cv2_to_imgmsg(image) 
         self.pub.publish(image_message) #publish the image
 
 
+        
     def cropMap(self, map):
-
-        xs, ys = np.where(map>=0)
-        result = map[min(xs):max(xs)+1,min(ys):max(ys)+1] 
-
-        return result
+        import scipy.sparse as sps
+        sparseM = sps.csr_matrix(map)
+        return sparseM[sparseM.getnnz(axis=1) != -1][:, sparseM.getnnz(axis=0) != -1].A
 
     def map2Image(self, map, start, end, path):
-        img = np.zeros((map.shape[0], map.shape[1], 3), np.int8)
+        img = np.zeros((map.shape[1], map.shape[0], 3), np.int8)
         path = np.asarray(path)
 
         # paint the freespace of white
         free_space_idx = np.where(map==1)
 
         for x, y in zip(free_space_idx[0], free_space_idx[1]): 
-            img[x,y,:] = 255
+            img[y,x,:] = 255
 
         #paint path
         for point in path:
-            img[point[0],point[1],0] = 0
-            img[point[0],point[1],1] = 255
-            img[point[0],point[1],2] = 0
+            img[point[1],point[0],0] = 0
+            img[point[1],point[0],1] = 255
+            img[point[1],point[0],2] = 0
         
         #Paint start
-        img[start[0],start[1],0] = 255
-        img[start[0],start[1],1] = 0
-        img[start[0],start[1],2] = 0
+        img[start[1],start[0],0] = 255
+        img[start[1],start[0],1] = 0
+        img[start[1],start[0],2] = 0
 
         #Paint end
-        img[end[0],end[1],0] = 0
-        img[end[0],end[1],1] = 0
-        img[end[0],end[1],2] = 255
+        img[end[1],end[0],0] = 0
+        img[end[1],end[0],1] = 0
+        img[end[1],end[0],2] = 255
         
         return img
+
+    def run(self):  
+        
+        # main thread will now be used to update periodically Astar path
+
+        sleepRate = rospy.Rate(500)
+
+        while(not rospy.is_shutdown()):
+            self.pathUpdateCallback()
+            sleepRate.sleep()
 
 def main():
 
     rospy.init_node('path_finding')
 
     pf = Path_finding()
-    rospy.spin()
+    pf.run()
 
 if __name__ == "__main__":
     try:
